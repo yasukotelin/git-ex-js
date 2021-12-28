d0d9b8f649acbb2ad42c4be743b25669949fe392
yasukotelin <31115673+yasukotelin@users.noreply.github.com>
2021-12-28 22:38:21 +0900

    Implements stage command

--- src/git/git.js
+++ src/git/git.js
@@ -2,6 +2,30 @@ import { execSync } from 'child_process';
 
 export class Git {
 
+    status = () => {
+        const results = execSync('git status --porcelain').toString().split(/\n/)
+
+        const status = {
+            stage: [],
+            unstage: [],
+            untracked: [],
+        }
+        results.forEach(e => {
+            if (!e.length) {
+                return
+            }
+            if (e.startsWith('??')) {
+                status.untracked.push(e.substring(3, e.length))
+            } else if (e.startsWith(' ')) {
+                status.unstage.push(e.substring(3, e.length))
+            } else {
+                status.stage.push(e.substring(3, e.length))
+            }
+        })
+        
+        return status
+    }
+
     getMergedBranches = () => {
         const stdout = execSync('git branch --merged')
         const branches = stdout.toString().split(/\n/)
@@ -17,6 +41,11 @@ export class Git {
     // Remove branches
     removeBranches = (branches) => {
         const stdout = execSync(`git branch -d ${branches.join(' ')}`)
+        return stdout.toString()
+    }
+
+    add = (files) => {
+        const stdout = execSync(`git add ${files.join(' ')}`)
         return stdout.toString()
     }
 }
